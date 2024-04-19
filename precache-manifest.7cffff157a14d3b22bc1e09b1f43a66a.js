fc785a05244e3def1f2d62cd983cacb67090a401
Vishnu Nakade <vishnunakade@gmail.com>
2024-04-19 14:10:24 +0530

    add

---
+++ precache-manifest.7cffff157a14d3b22bc1e09b1f43a66a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "a9903fdd2f7893f37448",
+    "url": "/static/css/main.d7379476.chunk.css"
+  },
+  {
+    "revision": "a9903fdd2f7893f37448",
+    "url": "/static/js/main.dbf21215.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "b0ab0d04c81883d619f7",
+    "url": "/static/js/2.e590551c.chunk.js"
+  },
+  {
+    "revision": "7c9f36f68cc0cb8c87967c7bcae95ee6",
+    "url": "/static/media/re4billion.7c9f36f6.png"
+  },
+  {
+    "revision": "42a00ce597b2835c205130d58a22a739",
+    "url": "/static/media/loginSound.42a00ce5.mp3"
+  },
+  {
+    "revision": "d4bc5046037ff64a8188a97e8015a38d",
+    "url": "/static/media/bg4.d4bc5046.png"
+  },
+  {
+    "revision": "aee3a1d0c9efdae2e44fef3d6dae86c5",
+    "url": "/index.html"
+  }
+];
